b31ccfb2d9702911bf09c94d170c6834a0e176ef
Alexander Wentzel <alexwentzel@live.dk>
2024-05-06 13:31:55 +0200

    first commit

---
+++ src/main/java/com/WebApplcation/MyDietPlan/MyDietPlanApplication.java
@@ -0,0 +1,13 @@
+package com.WebApplcation.MyDietPlan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDietPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDietPlanApplication.class, args);
+	}
+
+}
